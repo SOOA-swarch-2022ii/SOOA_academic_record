38aeed5a4bc2a27b742416a0cf461a914457ffa8
Diego Irreño <dirreno@unal.edu.co>
2022-10-02 17:19:25 -0500

    clean

--- SOOA_academic_record_ms/SOOA_academic_record_ms/serializers/course_record_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from SOOA_academic_record_ms.models.course_record_model import Course_record
-
-class Course_recordSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course_record
-        fields = ['course_id', 'course_name', 'course_topology', 'number_credits', 'course_grade']
